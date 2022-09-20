ad6a764f29441b2c871f97840d5d3ad7ad19cbe2
Maksym Kundeev <mkundeev@gmail.com>
2022-09-20 15:55:23 +0300

    createApi

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { testsApi } from "./testsApi";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [testsApi.reducerPath]: testsApi.reducer,
   },
 });
---
+++ src/app/testsApi.js
@@ -0,0 +1,62 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const testsApi = createApi({
+  reducerPath: "contacts",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://contacts-book-mk.herokuapp.com/",
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().currentUser.token;
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["Users"],
+  endpoints: (builder) => ({
+    registerUser: builder.mutation({
+      query(user) {
+        return {
+          url: `users/signup`,
+          method: "POST",
+          body: user,
+        };
+      },
+      invalidatesTags: ["Users"],
+    }),
+    authorizeUser: builder.mutation({
+      query(user) {
+        return {
+          url: `users/login`,
+          method: "POST",
+          body: user,
+        };
+      },
+      invalidatesTags: ["Users"],
+    }),
+    logOutUser: builder.mutation({
+      query() {
+        return {
+          url: `users/logout`,
+          method: "POST",
+        };
+      },
+      invalidatesTags: ["Users"],
+    }),
+    getUser: builder.query({
+      query() {
+        return {
+          url: `users/current`,
+        };
+      },
+      providesTags: ["Users"],
+    }),
+  }),
+});
+
+export const {
+  useAuthorizeUserMutation,
+  useRegisterUserMutation,
+  useLogOutUserMutation,
+  useGetUserQuery,
+} = testsApi;
