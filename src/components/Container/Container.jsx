b34246ac68f90079e1ffd53c05162fc63481c384
Maksym Kundeev <mkundeev@gmail.com>
2022-09-20 17:41:31 +0300

    Update Container.jsx

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import PropTypes from "prop-types";
 import s from "./Container.module.css";
 
 export default function Container({ children }) {
   return <div className={s.container}>{children}</div>;
 }
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+};
