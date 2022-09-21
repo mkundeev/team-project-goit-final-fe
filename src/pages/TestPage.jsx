1ff31e4d99e5d2e105d26a3e955c11b974b5fab2
Maksym Kundeev <mkundeev@gmail.com>
2022-09-21 16:50:47 +0300

    topic

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -4,11 +4,11 @@ import { useParams } from 'react-router-dom';
 import { useGetTestQuery } from 'app/testsApi';
 
 export default function TestPage() {
-  const { testId } = useParams();
-  const { data } = useGetTestQuery(testId);
+  const { topic } = useParams();
+  const { data } = useGetTestQuery(topic);
   console.log(data);
 
-  console.log(testId);
+  console.log(topic);
   return (
     <>
       <Test />
