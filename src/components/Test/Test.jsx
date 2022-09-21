bc62c570f21985b6efe77149a946b45a0e989490
Alina Lemishko <aliska.nofear@gmail.com>
2022-09-21 10:02:00 +0300

    added markup test

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -1,5 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsArrowRight, BsArrowLeft } from "react-icons/bs";
+import { questions } from "../TestCard/questions";
+import TestCard from "../TestCard/TestCard";
+import s from "./Test.module.css";
 
 export default function Test() {
-  return <div></div>;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const handleChangeDecrement = () => {
+    setCurrentQuestion(currentQuestion - 1);
+  };
+
+  const handleChangeIncrement = () => {
+    setCurrentQuestion(currentQuestion + 1);
+  };
+
+  return (
+    <div className={s.container}>
+      <div className={s.wrapTop}>
+        <div className={s.testNameWrap}>
+          <p className={s.testName}>[ Testing theory_ ]</p>
+        </div>
+        <button className={s.buttonFinish}>Finish test</button>
+      </div>
+      <TestCard currentQuestion={currentQuestion} />
+      <ul className={s.paginationBtnWrap}>
+        <li>
+          <button
+            className={s.paginationButton}
+            type="button"
+            disabled={currentQuestion === 0}
+            onClick={handleChangeDecrement}
+          >
+            <BsArrowLeft className={s.iconBtn} />
+            Previous question
+          </button>
+        </li>
+        <li>
+          <button
+            className={s.paginationButton}
+            type="button"
+            disabled={currentQuestion === questions.length - 1}
+            onClick={handleChangeIncrement}
+          >
+            Next question <BsArrowRight className={s.iconBtn} />
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
---
+++ src/pages/testPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Test from "../components/Test/Test";
+
+export default function TestPage() {
+  return (
+    <>
+      <Test />
+    </>
+  );
+}
