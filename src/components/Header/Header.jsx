19bb1254f3d9d9c1eedf46519c0885d1c7b2d954
Dmytrii_Shypilov <you@example.com>
2022-09-22 21:09:53 +0300

    Auth form + header primary features

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import s from './header.module.css'
+import logo from '../../images/header/logo.svg'
+import icon from '../../images/header/sign-out.svg'
+import Container from 'components/Container';
 import { NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { resetUser } from 'app/reducer';
@@ -13,55 +17,47 @@ export default function Header() {
       .unwrap()
       .then(() => dispatch(resetUser()));
   };
-  let activeStyle = {
-    textDecoration: 'underline',
-  };
-
-  let activeClassName = 'underline';
+ 
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink
-            to="home"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="test"
-            className={({ isActive }) =>
-              isActive ? activeClassName : undefined
-            }
-          >
-            Test
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="material">
-            {({ isActive }) => (
-              <span className={isActive ? activeClassName : undefined}>
-                Material
+    <header className={s.header}>
+      <Container>
+        <div className={s.headerWrapper}>
+          <img src={logo} className={s.logo} alt="" />
+          <div className={s.navBlock}>
+            <nav className={s.nav}>
+              <ul className={s.navList}>
+                <li className={s.navListItem}>
+                  <NavLink
+                    to="home"
+                    className={({ isActive }) => (isActive ? s.activeLink : s.navLink)}
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className={s.navListItem}>
+                  <NavLink to="material" className={({ isActive }) => (isActive ? s.activeLink : s.navLink)}>
+                    Materials
+                  </NavLink>
+                </li>
+                <li className={s.navListItem}>
+                  <NavLink to="contacts" className={({ isActive }) => (isActive ? s.activeLink : s.navLink)}>
+                    Contacts
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+            <div className={s.userPanel}>
+              <span className={s.userIcon}>D</span>
+              <span className={s.user}>Dmytrii</span>
+              <span className={s.signOutBtn} onClick={handleLogOut}>
+                <img className={s.signOutIcon} src={icon} alt="" />
               </span>
-            )}
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="contacts">
-            {({ isActive }) => (
-              <span className={isActive ? activeClassName : undefined}>
-                Contacts
-              </span>
-            )}
-          </NavLink>
-        </li>
-      </ul>
-      <button type="button" onClick={handleLogOut}>
-        LogOut
-      </button>
-    </nav>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </header>
+
   );
 }
