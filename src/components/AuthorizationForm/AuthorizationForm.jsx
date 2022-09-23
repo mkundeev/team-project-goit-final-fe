5682095b407135cd0a25c858338fc1da47002c21
Maksym Kundeev <mkundeev@gmail.com>
2022-09-23 10:30:37 +0300

    Update AuthorizationForm.jsx

--- src/components/AuthorizationForm/AuthorizationForm.jsx
+++ src/components/AuthorizationForm/AuthorizationForm.jsx
@@ -1,58 +1,91 @@
-import React from 'react'
-import s from "./auth-form.module.css"
-import { useState } from 'react'
-import icon from '../../images/AuthForm/google-icon.svg'
-import { useAuthorizeUserMutation } from 'app/testsApi';
-import { setUser } from 'app/reducer'
+import React from 'react';
+import s from './auth-form.module.css';
+import { useState } from 'react';
+import icon from '../../images/AuthForm/google-icon.svg';
+import {
+  useAuthorizeUserMutation,
+  useRegisterUserMutation,
+} from 'app/testsApi';
+import { setUser } from 'app/reducer';
 import { useDispatch } from 'react-redux';
 
 export default function AuthorizationForm() {
   const dispatch = useDispatch();
   const [authorizeUser] = useAuthorizeUserMutation();
+  const [registerUser] = useRegisterUserMutation();
   const [form, setForm] = useState({
     email: '',
     password: '',
-  })
+  });
 
-  const {email, password} = form
+  const { email, password } = form;
 
-  const onInput = (e) => {
+  const onInput = e => {
     setForm(prevState => {
       return {
         ...prevState,
         [e.target.id]: e.target.value,
-      }
-    })
-  }
+      };
+    });
+  };
 
   const logInUser = () => {
     authorizeUser({ password, form })
       .unwrap()
       .then(data => {
         dispatch(setUser(data));
-        console.log(data);
-      });
+      })
+      .catch(err => console.log(err.message));
+  };
+  const signUpUser = () => {
+    registerUser({ password, form })
+      .unwrap()
+      .then(() =>
+        authorizeUser({ password, form })
+          .unwrap()
+          .then(data => {
+            dispatch(setUser(data));
+          })
+      )
+      .catch(err => console.log(err.message));
   };
 
-  
   return (
     <div>
       <form className={s.form}>
         <p className={s.text}>You can use your Google Account to authorize:</p>
         <div className={s.link}>
-          <img className={s.googleIcon} src={icon} alt=""/>
+          <img className={s.googleIcon} src={icon} alt="" />
           <span className={s.googleTxt}>Google</span>
         </div>
         <p className={s.text}>Or login to our app using e-mail and password:</p>
         <div className={s.inputBlock}>
-          <input onChange={onInput} id="email" value={email} className={s.input} type="text" placeholder="E-mail" />
-          <input onChange={onInput} id="password" value={password} className={s.input} type="text" placeholder="Password" />
+          <input
+            onChange={onInput}
+            id="email"
+            value={email}
+            className={s.input}
+            type="text"
+            placeholder="E-mail"
+          />
+          <input
+            onChange={onInput}
+            id="password"
+            value={password}
+            className={s.input}
+            type="text"
+            placeholder="Password"
+          />
         </div>
         <div>
-          <button onClick={logInUser} className={s.btn}>Sign in</button>
-          <button className={s.btn}>Sign up</button>
+          <button onClick={logInUser} className={s.btn}>
+            Sign in
+          </button>
+          <button onClick={signUpUser} className={s.btn}>
+            Sign up
+          </button>
         </div>
       </form>
     </div>
-  )
+  );
 }
