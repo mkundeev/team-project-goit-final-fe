86fc739dd3a9c79f8fa880341fe1801f9952af18
Maksym Kundeev <mkundeev@gmail.com>
2022-09-28 21:58:12 +0300

    fix header

--- src/app/store.js
+++ src/app/store.js
@@ -17,6 +17,7 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
   key: 'user',
   storage,
+  whitelist: ['token'],
 };
 const persReducer = persistReducer(persistConfig, userSlice.reducer);
 
